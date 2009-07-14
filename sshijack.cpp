0741331f0bf5efa298ed801e68c12be4d6573190
lukasz <lukasz@26da5a51-cf2d-458a-be3d-5cea51cc0eb7>
2009-07-14 21:51:47 +0000

    cleanup
    git-svn-id: file:///sshijack/sshijack@3 26da5a51-cf2d-458a-be3d-5cea51cc0eb7

--- sshijack.cpp
+++ sshijack.cpp
@@ -18,10 +18,10 @@ inline unsigned int getValue(unsigned int addr, int tracepid)
 inline int writeLong(unsigned int addr, unsigned long value, int tracepid)
 {
 	int retval = ptrace(PTRACE_POKEDATA,tracepid,addr, value);
-	if(retval == -1)
+	/*	if(retval == -1)
 		perror("ptrace write");
 	else
-		printf("Write OK!");
+		printf("Write OK!");*/
 	return retval;
 }
 
@@ -56,9 +56,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
-	
-	//long ptrace(enum __ptrace_request request, pid_t pid, void *addr, void *data);
-	
 	if(ptrace(PTRACE_ATTACH, pid, NULL, NULL) == -1)
 	{
 		perror("PTRACE_ATTACH");
@@ -78,10 +75,11 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 	
-	int wyjscie = 0;
+	int inSyscall = 0;
 	
 	while(1)
 	{	
+		// We are interested only in syscalls
 		if(ptrace(PTRACE_SYSCALL, pid, NULL, NULL) == -1)
 		{
 			perror("PTRACE_SYSCALL");
@@ -93,16 +91,18 @@ int main(int argc, char *argv[])
 			printf("Process exited\n");
 			return 0;
 		}
-				
+		
+		
 		struct user_regs_struct regs;
+		// TODO: check retval
 		ptrace((__ptrace_request)PTRACE_GETREGS, pid, 0, &regs);
-		printf("__EAX: %d (orig: %d)\n", regs.eax, regs.orig_eax);
-		printf("Status: %x\n", status);
+		/*printf("__EAX: %d (orig: %d)\n", regs.eax, regs.orig_eax);
+		printf("Status: %x\n", status);*/
 		if(regs.orig_eax == SYS_read || regs.orig_eax == -1/*wyjscie == 1*/)
 		{
 			
 			
-			int fd, buf, count;
+			/*
 			int tbl[100];
 			printf("EIP: %x\tESP: %x\tEBP: %x\tEAX: %x\tEBX: %x\tECX: %x\tEDX: %x\n", regs.eip, regs.esp, regs.ebp, regs.eax, regs.ebx, regs.ecx, regs.edx);
 			printf("EAX_orig: %x\n", regs.orig_eax);
@@ -112,35 +112,46 @@ int main(int argc, char *argv[])
 				printf("%08x ", tbl[i]);
 			}
 			printf("\n");
+			*/
 			
-			if(wyjscie == 0)
+			if(inSyscall == 0)
 			{
-				printf("======== Run 1 ========\n");
+				// First ptrace trap. We are about to run a syscall.
+				// Remember it and change it to a nonexisting one
+				// Then wait for ptrace to stop execution again after
+				// running it.
+				inSyscall = regs.orig_eax;
+				
+				// This syscall can't exist :)
 				regs.orig_eax = -1;
 				regs.eax = -1;
 				
+				// TODO: check retval
 				ptrace((__ptrace_request)PTRACE_SETREGS, pid, 0, &regs);
-				wyjscie = 1;
 			}
-			else if(wyjscie == 1)
+			else
 			{
-				printf("======== Run 2 ========\n");
-				writeChar(regs.ecx, 'x', pid);
-				//regs.orig_eax = -1;
-				regs.eax = 1;
-				
+				// Second ptrace trap. We just finished running our
+				// nonexisting syscall. Now is the moment to inject the
+				// "correct" return values, etc...
+				switch(inSyscall)
+				{
+				case SYS_read:
+					writeChar(regs.ecx, 'x', pid);
+					//regs.orig_eax = -1;
+					regs.eax = 1;
+					break;
+					
+				}
+				// TODO: check retval
 				ptrace((__ptrace_request)PTRACE_SETREGS, pid, 0, &regs);
-				wyjscie = 0;
+				inSyscall = 0;
 			}
-			else printf("*********  Blad w ifach!!!  *********\n");
-			
-			
-			
-			printf("End\n");	
+		
 		}
 		
 	}
-	
+	// TODO: check retval
 	ptrace(PTRACE_DETACH, pid, NULL, NULL);
 	
 	

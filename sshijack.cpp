b0704717c3ce35dbf4be2dd7c2e8331c8581c949
Lukasz Taczuk <taczuk@gmail.com>
2010-10-26 14:56:27 +0200

    Arch detection and calling conventions

--- sshijack.cpp
+++ sshijack.cpp
@@ -13,20 +13,29 @@
 
 void ex_program(int sig);
 
-#if 0 /* insert 32bit test here */
+#if defined(__i386__) || defined(M_IX86) /* x86 arch */
+//#warning x86 architecture detected
 
 #define ARG1 ebx
 #define ARG2 ecx
 #define ARG3 edx
-//#define ARG4
+#define ARG4 esi
+#define ARG5 edi
+#define ARG6 ebp
 
-#else /* x86_64 arch */
+#elif defined(__amd64__) || defined(_M_X64) /* x86_64 arch */
+//#warning x86_64 architecture detected
 
 #define ARG1 rdi
 #define ARG2 rsi
 #define ARG3 rdx
 #define ARG4 rcx
+#define ARG5 r8
+#define ARG6 r9
+//Note to self: Kernel-kernel calls use rdi, rsi, rdx, r10, r8, r9
 
+#else
+#error "Can't recognize processor architecture!"
 #endif
 
 using namespace std;
